fa8982a34b2e86a1a7bb93cf6d87b21b7489b5d2
Jenny Takahara <jenny.takahara@gmail.com>
2023-11-24 20:08:04 -0800

    initial commit and adding algolia

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  DynamicWidgets,
+  InstantSearch,
+  Hits,
+  Highlight,
+  RefinementList,
+  SearchBox,
+  InstantSearchServerState,
+  InstantSearchSSRProvider,
+  getServerState,
+} from "react-instantsearch";
+import algoliasearch from "algoliasearch/lite";
+import { Hit as AlgoliaHit } from "instantsearch.js";
 
+const client = algoliasearch("TV0WINJGZU", "ff9e9ae7119e5a8381082aa6fcc757d4");
+
+function Hit({ hit }) {
+  return (
+    <article>
+      <img src={hit.image} alt={hit.name} />
+      <p>{hit.categories[0]}</p>
+      <h1>
+        {" "}
+        <Highlight attribute="name" hit={hit} />
+      </h1>
+      <p>${hit.price}</p>
+    </article>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +45,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <InstantSearch searchClient={client} indexName="pandabuy">
+        <SearchBox />
+        <RefinementList attribute="brand" />
+        <RefinementList attribute="price" />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
     </div>
   );
 }
