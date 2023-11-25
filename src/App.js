a42ee32fd6c20e9673a9ac1c859b2f155f082cd8
Jenny Takahara <jenny.takahara@gmail.com>
2023-11-24 23:48:48 -0800

    adding filters and styling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   InstantSearchServerState,
   InstantSearchSSRProvider,
   getServerState,
+  Pagination,
 } from "react-instantsearch";
 import algoliasearch from "algoliasearch/lite";
 import { Hit as AlgoliaHit } from "instantsearch.js";
@@ -20,13 +21,12 @@ const client = algoliasearch("TV0WINJGZU", "ff9e9ae7119e5a8381082aa6fcc757d4");
 function Hit({ hit }) {
   return (
     <article>
-      <img src={hit.image} alt={hit.name} />
-      <p>{hit.categories[0]}</p>
+      <img src={hit.image_url} alt={hit.name} />
       <h1>
-        {" "}
         <Highlight attribute="name" hit={hit} />
       </h1>
       <p>${hit.price}</p>
+      <a href={hit.link}>CLICK TO COP</a>
     </article>
   );
 }
@@ -47,10 +47,11 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <InstantSearch searchClient={client} indexName="pandabuy">
+      <InstantSearch searchClient={client} indexName="pandabuy_prod">
         <div className="Container">
           <div>
             <RefinementList attribute="brand" />
+            <RefinementList attribute="name" />
             <RefinementList attribute="price" />
           </div>
           <div>
@@ -59,6 +60,7 @@ function App() {
             <Hits hitComponent={Hit} />
           </div>
         </div>
+        <Pagination />
       </InstantSearch>
     </div>
   );
